48e206b318d213b97edb3596c4f184aadcc89e20
viccyrod <victor@gebarski.com>
2022-11-28 19:06:30 +0100

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,29 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = 
+`
+This is a conversation with a lawyer and a client. The lawyer answers the questions in relation to Australian law.
+
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.75,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
